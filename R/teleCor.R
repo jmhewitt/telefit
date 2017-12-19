e14eb3c62ebcb16106dec828a6d8df9779d3d799
Josh <joshua.hewitt@colostate.edu>
2017-12-19 14:46:00 -0700

    optimized teleCor

--- R/teleCor.R
+++ R/teleCor.R
@@ -19,30 +19,9 @@ teleCor = function( stData = NULL, Y = stData$Y, Z = stData$Z,
                     coords.s = stData$coords.s, coords.r = stData$coords.r, 
                     ncores = 1 ) {
   
-  ny = nrow(Y)
-  nz = nrow(Z)
-  
-  # set up basic parallel backend if none is registered
-  if(!getDoParRegistered()) {
-    registerDoMC(ncores)
-  }
-  
-  # compute pointwise correlation of local variate with each remote location
-  # (use chunkSize to exactly split tasks across ncores)
-  chunkSize = ceiling(ny/ncores)
-  res = foreach(inds = ichunk(1:ny, chunkSize = chunkSize), .combine = 'rbind',
-                .export = c('coords.s', 'coords.r', 'Y', 'Z')) %dopar% {
-    foreach(y = unlist(inds), .combine='rbind') %do% {
-    foreach(z = 1:nz, .combine='rbind') %do% {
-      data.frame( lon.Y = coords.s[y,1], lat.Y = coords.s[y,2],
-                  lon.Z = coords.r[z,1], lat.Z = coords.r[z,2],
-                  cor = cor(Y[y,], Z[z,]) )
-    }
-  }}
-  
   # coerce to a more compact form
   res = list(
-    cor = matrix(res$cor, nrow=ny, byrow = T),
+    cor = cor(t(Y), t(Z)),
     coords.r = coords.r,
     coords.s = coords.s
   )
